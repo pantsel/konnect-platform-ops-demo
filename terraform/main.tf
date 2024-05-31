c9617a2dc7d6f5233618b36b276c57d36b7a3f31
pantsel <panagis.tselentis@konghq.com>
2024-05-31 14:44:38 +0200

    Provision CP group

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,32 +13,64 @@ provider "konnect" {
   server_url            = var.konnect_server_url
 }
 
-resource "konnect_gateway_control_plane" "tfdemo" {
-  name         = "Demo CP"
+resource "konnect_gateway_control_plane" "tfcpgroup" {
+  name         = "Demo CP Group"
+  description  = "This is a demo Control Plane Group"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE_GROUP"
+  auth_type    = "pki_client_certs"
+
+  proxy_urls = []
+
+    labels = {
+    env          = "demo",
+    team         = "platform",
+    generated_by = "terraform"
+  }
+
+}
+
+resource "konnect_gateway_control_plane" "tfglobalcp" {
+  name         = "Demo Global CP"
   description  = "This is a demo Control Plane"
   cluster_type = "CLUSTER_TYPE_HYBRID"
   auth_type    = "pki_client_certs"
   labels = {
     env          = "demo",
-    team         = "tfdemo",
+    team         = "platform",
     generated_by = "terraform"
   }
 
-  proxy_urls = [
-    {
-      host     = "example.com",
-      port     = 443,
-      protocol = "https"
-    }
+  proxy_urls = []
+}
+
+resource "konnect_gateway_control_plane" "tfteamcp" {
+  name         = "Demo Team CP"
+  description  = "This is a demo Control Plane"
+  cluster_type = "CLUSTER_TYPE_HYBRID"
+  auth_type    = "pki_client_certs"
+  labels = {
+    env          = "demo",
+    team         = "team1",
+    generated_by = "terraform"
+  }
+
+  proxy_urls = []
+}
+
+resource "konnect_gateway_control_plane_membership" "gatewaycontrolplanemembership" {
+  id = konnect_gateway_control_plane.tfcpgroup.id
+  members = [
+    { id = konnect_gateway_control_plane.tfglobalcp.id },
+    { id = konnect_gateway_control_plane.tfteamcp.id }
   ]
 }
 
-# Add the required data plane certificates 
+# Add the required data plane certificates to the control plane group
 resource "konnect_gateway_data_plane_client_certificate" "demo_ca_cert" {
   cert             = file("../.tls/ca.crt")
-  control_plane_id = konnect_gateway_control_plane.tfdemo.id
+  control_plane_id = konnect_gateway_control_plane.tfcpgroup.id
 }
 
 output "kong_gateway_control_plane_info" {
-  value = konnect_gateway_control_plane.tfdemo
+  value = konnect_gateway_control_plane.tfcpgroup
 }
