710f8f80a7ea3f449e03247204c6af253b88bfb2
pantsel <panagis.tselentis@konghq.com>
2024-06-11 14:54:02 +0200

    wip

---
+++ node_modules/@stoplight/spectral-owasp-ruleset/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = async () => {
+  return {
+    preset: "ts-jest",
+    testPathIgnorePatterns: ["__helpers__"],
+    testEnvironment: "node",
+    globals: {
+      "ts-jest": {
+        useIsolatedModules: true,
+      },
+    },
+  };
+};
---
+++ node_modules/@stoplight/spectral-runtime/node_modules/@stoplight/types/dist/node.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Stoplight node types
+ */
+export declare enum NodeType {
+    Article = "article",
+    HttpService = "http_service",
+    HttpServer = "http_server",
+    HttpOperation = "http_operation",
+    Model = "model",
+    Generic = "generic",
+    Unknown = "unknown",
+    TableOfContents = "table_of_contents",
+    SpectralRuleset = "spectral_ruleset",
+    Styleguide = "styleguide"
+}
+/**
+ * Node data formats
+ */
+export declare enum NodeFormat {
+    Json = "json",
+    Markdown = "markdown",
+    Yaml = "yaml"
+}
