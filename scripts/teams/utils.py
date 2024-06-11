acf19d838d3b909e4c774775aec3c2ccf75d2d3c
pantsel <panagis.tselentis@konghq.com>
2024-06-11 15:16:31 +0200

    Fetch credentials from vault

--- scripts/teams/utils.py
+++ scripts/teams/utils.py
@@ -18,11 +18,6 @@ def merge_arrays(arr1, arr2):
     return merged_list
 
 def validate_config(config, logging):
-    if "_format_version" not in config or "_type" not in config or "name" not in config or "region" not in config or "description" not in config or "resources" not in config or "plan" not in config:
-        logging.error("Config file should have '_format_version', '_type', 'name', 'region', 'description', 'resources', and 'plan' keys.")
-        exit(1)
-    if not isinstance(config["_format_version"], str) or not is_valid_semver(config["_format_version"]):
-        logging.error("_format_version should be a valid semver string.")
-        exit(1)
+    # TODO: Add more validation checks
     
     return True
