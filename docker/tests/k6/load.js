45aa768809c8cf0b1845ad73fdad26c58591f087
pantsel <panagis.tselentis@konghq.com>
2024-11-13 13:27:21 +0100

    ref

--- docker/tests/k6/load.js
+++ docker/tests/k6/load.js
@@ -10,7 +10,7 @@ export let options = {
     vus: vus, // Number of virtual users to simulate
     duration: duration, // Duration of the test
     thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_failed: ['rate<0.01'], // http errors should be less than 0.11%
         http_req_duration: ['p(95)<500'], // 95% of requests must complete within 500ms
     }
 };
