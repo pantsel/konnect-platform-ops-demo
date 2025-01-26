4883f5ef36adeab65c885349e54edc68fb91d223
pantsel <panagis.tselentis@konghq.com>
2025-01-26 22:42:40 +0100

    wip konnect provisioning ref

--- terraform/konnect/main.tf
+++ terraform/konnect/main.tf
@@ -25,19 +25,36 @@ locals {
   }
 }
 
+# module "control_planes" {
+#   source         = "./modules/control-planes"
+#   environment    = var.environment
+#   metadata       = local.metadata
+#   control_planes = local.control_planes
+#   cacert         = var.cacert
+# }
+
 module "control_planes" {
-  source         = "./modules/control-planes"
-  environment    = var.environment
-  metadata       = local.metadata
-  control_planes = local.control_planes
-  cacert         = var.cacert
+  source = "./modules/control_plane"
+
+  for_each = { for cp in local.control_planes : cp.name => cp }
+
+  name         = each.value.name
+  description  = each.value.description
+  labels       = lookup(each.value, "labels", {})
+  cluster_type = lookup(each.value, "cluster_type", "CLUSTER_TYPE_HYBRID")
+  auth_type    = lookup(each.value, "auth_type", "pki_client_certs")
+  cacert       = var.cacert
 }
 
 module "api_products" {
-  source       = "./modules/api-products"
-  environment  = var.environment
-  metadata     = local.metadata
-  api_products = local.api_products
+  source = "./modules/api_product"
+
+  for_each = { for product in local.api_products : product.name => product }
+
+  name          = each.value.name
+  description   = each.value.description
+  labels        = lookup(each.value, "labels", {})
+  public_labels = lookup(each.value, "public_labels", {})
 }
 
 module "team" {
@@ -46,16 +63,23 @@ module "team" {
   name           = title(local.metadata.name)
   description    = lookup(local.metadata, "description", "")
   region         = lookup(local.metadata, "region", "")
-  control_planes = module.control_planes.control_planes
-  api_products   = module.api_products.api_products
+  control_planes = [for k, v in module.control_planes : v.control_plane]
+  api_products   = [for k, v in module.api_products : v.api_product]
 }
 
 module "system_accounts" {
   source = "./modules/system_account"
 
   for_each = merge(
-    { for cp in module.control_planes.control_planes : cp.name => { name = cp.name, id = cp.id, type = "Control Planes" } },
-    { for product in module.api_products.api_products : product.name => { name = product.name, id = product.id, type = "API Products" } }
+    { for k, v in module.control_planes : v.control_plane.name => {
+      name = v.control_plane.name, id = v.control_plane.id
+      type = "Control Planes"
+    } },
+
+    { for k, v in module.api_products : v.api_product.name => {
+      name = v.api_product.name, id = v.api_product.id
+      type = "API Products"
+    } }
   )
 
   name             = lower(replace("sa_${each.value.name}_${local.short_names[each.value.type]}_admin", " ", "_"))
