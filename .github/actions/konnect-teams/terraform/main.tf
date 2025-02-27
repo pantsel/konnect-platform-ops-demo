3eaa9436cc398db1ef1c1ec2b41ad874a343ed66
pantsel <panagis.tselentis@konghq.com>
2025-02-27 20:07:26 +0100

    Create sas and roles on teams onboarding

--- .github/actions/konnect-teams/terraform/main.tf
+++ .github/actions/konnect-teams/terraform/main.tf
@@ -20,3 +20,46 @@ resource "konnect_team" "this" {
   }))
   name = each.value.name
 }
+
+### Foreach team, create system accounts
+resource "konnect_system_account" "this" {
+  for_each = { for team in local.teams : team.name => team }
+
+  name = "sa-${replace(lower(each.value.name), " ", "-")}"
+  description = "System account for creating control planes for the ${each.value.name} team"
+  
+  konnect_managed  = false
+}
+
+# Assign the system accounts to the teams
+resource "konnect_system_account_team" "this" {
+  for_each = { for team in konnect_team.this : team.name => team }
+
+  team_id =  each.value.id
+
+  account_id = {
+    for sa in konnect_system_account.this : sa.name => sa.id
+  }["sa-${replace(lower(each.value.name), " ", "-")}"]
+}
+
+### Add the control plane creator role to every team system account
+resource "konnect_system_account_role" "cp_creators" {
+  for_each = { for sa in konnect_system_account.this : sa.name => sa }
+
+  entity_id = "*"
+  entity_region    = "eu" # Hardcoded for now
+  entity_type_name = "Control Planes"
+  role_name        = "Creator"
+  account_id = each.value.id
+}
+
+### Add the api product creator role to every team system account
+resource "konnect_system_account_role" "ap_creators" {
+  for_each = { for sa in konnect_system_account.this : sa.name => sa }
+
+  entity_id = "*"
+  entity_region    = "eu" # Hardcoded for now
+  entity_type_name = "API Products"
+  role_name        = "Creator"
+  account_id = each.value.id
+}
