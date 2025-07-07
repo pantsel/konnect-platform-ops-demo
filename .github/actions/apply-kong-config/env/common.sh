2f896e1d18e47c794ae43dd71f9dcf5a3ec586c9
battlebyte <jordi@fernandez.name>
2025-07-07 16:53:27 +0200

    Refactor environment variable sourcing: replace inline sourcing with reusable action and update common.sh for consistency

--- .github/actions/apply-kong-config/env/common.sh
+++ .github/actions/apply-kong-config/env/common.sh
@@ -1,2 +1,4 @@
 export GATEWAY_URL="http://localhost"
 export KONNECT_SERVER_URL="https://eu.api.konghq.com"
+export VAULT_ADDR="http://localhost:8300"
+export KONNECT_REGION="eu"
--- .github/actions/build-kong-config/env/common.sh
+++ .github/actions/build-kong-config/env/common.sh
@@ -1,2 +1,4 @@
 export DECK_CONFIG_OIDC_ISSUER="http://192.168.178.42:8080/realms/demo/.well-known/openid-configuration"
 export KONNECT_SERVER_URL="https://eu.api.konghq.com"
+export VAULT_ADDR="http://localhost:8300"
+export KONNECT_REGION="eu"
